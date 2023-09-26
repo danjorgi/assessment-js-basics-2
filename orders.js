bb66023eec6cb808e8db40783665fed79248b151
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-09-26 07:12:53 -0600

    Orders: Problem 1 complete

--- orders.js
+++ orders.js
@@ -30,7 +30,18 @@
 */
 
 //CODE HERE
-
+class Ticket {
+    constructor(items, orderTime, customerID) {
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerID = customerID;
+        this.status = 'queued';
+    }
+    updateStatus(newStatus) {
+        this.status = newStatus;
+        console.log(`The order for customer ${this.customerID} is now ${newStatus}.`)
+    }
+}
 
 
 /*
@@ -45,7 +56,7 @@
 */
 
 //CODE HERE
-
+let firstTicket = new Ticket(['pizza', 'bread', 'soda'], '7:03 PM', 575)
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +65,5 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus('cooking')
+console.log(firstTicket)
