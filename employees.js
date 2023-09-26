f4f9381a1853e13876aa8ecba3e124add149a493
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-09-26 07:21:46 -0600

    Employees: Problem 1 complete

--- employees.js
+++ employees.js
@@ -20,7 +20,15 @@
 */
 
 //CODE HERE
-
+class Employee {
+    constructor(name, shifts) {
+        this.name = name;
+        this.shifts = shifts;
+    }
+    getSchedule() {
+        console.log(`${this.name} works on ${this.shifts}.`)
+    }
+}
 
 
 /*
@@ -34,14 +42,14 @@
 */
 
 //CODE HERE
-
+let empOne = new Employee('Jess', ['weekday mornings', 'weekday afternoons'])
 /*
     Call the `getSchedule` method on the
     `empOne` object.
 */
 
 //CODE HERE
-
+empOne.getSchedule()
 
 /*
     Make a copy of the empOne object
@@ -56,7 +64,7 @@
 */
 
 //CODE HERE
-
+let empTwo = {...empOne, name: 'Nick'};
 
 
 //////////////////PROBLEM 2////////////////////
